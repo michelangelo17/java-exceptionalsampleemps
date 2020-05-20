8300db3b406a9bf6c97f16373e986e093dc3f04d
Lambda John <lambdajohn@Johns-MBP.wavecable.com>
2020-05-19 17:34:24 -0700

    Updating Documentation

---
+++ exceptionalsampleemps_initial/src/main/java/com/lambdaschool/sampleemps/services/EmployeeService.java
@@ -0,0 +1,24 @@
+package com.lambdaschool.sampleemps.services;
+
+import com.lambdaschool.sampleemps.models.Employee;
+
+import java.util.List;
+
+public interface EmployeeService
+{
+    List<Employee> findAllEmployees();
+
+    Employee findEmployeeById(long id);
+
+    List<Employee> findEmployeeNameContaining(String subname);
+
+    List<Employee> findEmployeeEmailContaining(String subemail);
+
+    Employee save(Employee employee);
+
+    Employee update(
+        Employee employee,
+        long employeeid);
+
+    void delete(long employeeid);
+}
